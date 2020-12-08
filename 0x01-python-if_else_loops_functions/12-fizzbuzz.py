f65a88812d3c4cc662d748aae5d76eb2713287a2
ranicholson <ranicho2@gmail.com>
2020-12-08 16:55:47 -0600

    Task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def fizzbuzz():
+
+    x = 0
+
+    while x <= 100:
+        if (x % 5) == 0 and (x % 3) == 0:
+            print("FizzBuzz ", end="")
+
+        elif (x % 3) == 0:
+            print("Fizz ", end="")
+
+        elif (x % 5) == 0:
+            print("Buzz ", end="")
+
+        else:
+            print("{} ".format(x), end="")
+
+        x += 1
+
+    print()
