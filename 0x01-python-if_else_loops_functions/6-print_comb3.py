aa3c5e9d6a3825302bff2e75220ceac6aa35d597
ranicholson <ranicho2@gmail.com>
2020-12-08 15:35:15 -0600

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,7 +5,7 @@ for x in range (0, 10):
         if (y - x) <= 0:
             continue
         elif y == 9 and x == 8:
-            print("89")
+            print("{}{}".format(x, y)
             break
         else:
             print("{}{}".format(x, y), end=", ")
