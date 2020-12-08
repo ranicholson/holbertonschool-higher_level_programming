c19e70665d6ff064dfb1f4898007ce081b823d81
ranicholson <ranicho2@gmail.com>
2020-12-08 15:50:31 -0600

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-for x in range (0, 10):
-    for y in range (1, 10):
+for x in range(0, 10):
+
+    for y in range(1, 10):
         if (y - x) <= 0:
             continue
         elif y == 9 and x == 8:
