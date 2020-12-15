ab89e6094a3399f45cd57b7d01ad03380706238f
ranicholson <ranicho2@gmail.com>
2020-12-15 17:49:02 -0600

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,12 +4,10 @@
 def print_matrix_integer(matrix=[[]]):
 
     if matrix is not None:
-        y = 0
         for x in matrix:
             for y in x:
-                print("{:d} ".format(y), end="")
-            if y == (len(matrix) - 1):
-                print()
-            else:
-                print(" ")
-                y += 1
+                print("{:d}".format(y), end="")
+                if y != x[-1]:
+                    print(" ", end="")
+
+            print()
