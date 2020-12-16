ddfaa946d35f134ca229ee40dcd6134ded908875
ranicholson <ranicho2@gmail.com>
2020-12-15 19:03:07 -0600

    Task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+
+    nlist = my_list.copy()
+
+    for x in nlist:
+        if x % 2 == 0 and x != 0:
+            nlist[x] = True
+
+        else:
+            nlist[x] = False
+
+    return nlist
