fec7c762fa6d524d41bd2411dec2b06c5c4a49aa
ranicholson <ranicho2@gmail.com>
2020-12-18 13:36:44 -0600

    Task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,3 +5,11 @@ def weight_average(my_list=[]):
 
     if len(my_list) == 0:
         return 0
+
+    tot = 0
+    wtot = 0
+    for i in range(0, len(my_list)):
+        tot += int(my_list[i][0]) * int(my_list[i][1])
+        wtot += int(my_list[i][1])
+
+    return (tot/wtot)
