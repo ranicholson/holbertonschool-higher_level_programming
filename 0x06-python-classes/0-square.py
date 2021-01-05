a5aa564ac61bb4ab7c133004ae823f2413870f58
ranicholson <ranicho2@gmail.com>
2021-01-05 16:26:52 -0600

    Task 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
+"""Defining a square class"""
+
 
 class Square:
+
+    """Empty class to represent a square"""
 
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 
+"""Create a square class"""
 
 class Square:
 
+    """Create a square object"""
+
     def __init__(self, size=None):
+
+        """
+        Use __init__ to initialize square with private size
+        Args:
+             __Square.size (int): private size of square
+        """
+
         self.__Square_size = size
