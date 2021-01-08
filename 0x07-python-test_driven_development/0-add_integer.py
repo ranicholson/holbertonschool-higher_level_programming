1648b74ad9db4d59ee8944563e2545c23b4b2b9f
ranicholson <ranicho2@gmail.com>
2021-01-08 12:02:51 -0600

    Task 0 function

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""Function to add two integers"""
+
+
+def add_integer(a, b=98):
+
+    """Returns the sum of two integers.
+
+    Accepts float as well, but must be converted to int.
+    Args:
+        a (int/float): first argument
+        b (int/float): second argument
+    Return:
+        Sum of a + b
+    Raises:
+        TypeError: Occurs if a or b is an integer or float.
+    """
+
+    if isinstance(a, int) is False:
+        if isinstance(a ,float) is True:
+            a = int(a)
+
+        else:
+            raise TypeError("a must be an integer")
+
+    if isinstance(b, int) is False:
+        if isinstance(b, float) is True:
+            b = int(b)
+
+        else:
+            raise TypeError("b must be an integer")
+
+    return (a + b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,11 @@
+# 0x07. Python - Test-driven development
+
+Taking the time to test.
+
+## How can you practice this same project?
+
+Join up with Holberton's next cohort, and you can do this exact same or similar exercise!
+
+## Questions or Comments
+
+[Allen Nicholson](https://github.com/ranicholson)
