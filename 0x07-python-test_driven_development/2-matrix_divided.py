67a53df9da2165d9256523807480636290e7b026
ranicholson <ranicho2@gmail.com>
2021-01-08 20:43:07 -0600

    Task 1

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""Function that divides all elements of a matrix"""
+
+
+def matrix_divided(matrix, div):
+
+    """Divide elements of a matrix
+    Args:
+        matrix (list): matrix of elements to divide
+        div (int/float): integer to divide elements by
+    Raises:
+        TypeError: Occurs if elements of the matrix or div are not integers or
+            floats.Also occurs if rows of the matrix are not the same size.
+        ZeroDivisionError: Occurs when div is 0
+    Return: New matrix with values of each element divided by div.
+    """
+
+    if isinstance(all(matrix), (int, float)) is False or not matrix:
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        " of integers/floats")
+
+    if isinstance(div, (float, int)) is False or div == 0:
+        if div == 0:
+            raise ZeroDivisionError("division by zero")
+        else:
+            raise TypeError("div must be a number")
+
+    nlist = []
+    clist = []
+    rowlen = len(matrix[0])
+
+    for x in matrix:
+        if len(x) != rowlen:
+            raise TypeError("Each row of the matrix must have the same size")
+        for y in x:
+            nlist.append(round((y / div), 2))
+        rowlen = len(x)
+        clist.append(nlist)
+        nlist = []
+
+    return (clist)
