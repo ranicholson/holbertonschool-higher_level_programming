7d05bb765bc55a1962b027fee4b88ec2ca95ef98
ranicholson <ranicho2@gmail.com>
2021-01-08 22:27:01 -0600

    task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""Function that inserts two lines after ., ?, or :"""
+
+
+def text_indentation(text):
+
+    """Function to print two lines after specified characters.
+    Args:
+        text (str): text to print
+    Raises:
+        TypeError: Raised if anything other than a string is passed
+    Return: None
+    """
+
+    y = 0
+
+    if isinstance(text, str) is False:
+        raise TypeError("text must be a string")
+
+    for x in text:
+        if y == 1:
+            y = 0
+            continue
+
+        print(x, end="")
+        if x == "." or x == "?" or x == ":":
+            print("\n")
+            y = 1
+
+    print()
