38b145b6bbce08cd2a2036f8ef511605cf78f063
ranicholson <ranicho2@gmail.com>
2021-01-11 14:06:25 -0600

    Task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+"""Defining the Rectangle class"""
+
+
+class Rectangle:
+
+    """Attributes that make up the Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """Initializing of a new Rectangle.
+        Args:
+            width (int): Width of Rectangle. Value will be 0 if not specified.
+            height (int): Height of Rectangle Value will be 0 if not specified.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Function that retrieves width of Rectangle."""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Function that sets the value of the width of a Rectangle:
+        Args:
+            value (int): Value for the width. Must be int >= 0.
+        Raises:
+            TypeError: must be integer
+            ValueError: muist be >= 0
+        """
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Function that retrieves the height of a Rectangle"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Function that sets the height of a Rectangle
+        Args:
+            value (int): Value of the height. Must be int >= 0
+        Raises:
+            TypeError: must be an integer
+            ValueError: must be >= 0
+        """
+        if isinstance(value, int) is False:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Function that returns the area of a Rectangle"""
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        """Function that returns the perimeter of a Rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """Prints a rectangle using the # chracter"""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        prow = self.__width * '#'
+        nstr = ""
+        for x in range(self.__height):
+            nstr += prow
+            if x == (self.__height - 1):
+                break
+            nstr += "\n"
+
+        return (nstr)
