ef939b97d3a81365292490e7f4c2ddf10b163515
ranicholson <ranicho2@gmail.com>
2021-01-19 14:33:37 -0600

    task 4:

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Function that returns true if object is an instance of an object inherited
+from the specified class"""
+
+
+def inherits_from(obj, a_class):
+    """Function that checks if the object is a class that has inheritance
+    from the specified class.
+    Args:
+        obj (obj): object to check
+        a_class (class): class to check object
+    Return:
+        True: if it is true
+        False: if it is false
+    """
+
+    if isinstance(obj, a_class) is True and type(obj) != a_class:
+        return (True)
+    else:
+        return (False)
