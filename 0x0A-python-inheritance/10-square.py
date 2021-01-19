19b0dc78c123c5bbffa23165a4f516e7ad262b7e
ranicholson <ranicho2@gmail.com>
2021-01-19 15:42:02 -0600

    Task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Define Square subclass with inheritance from Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Methods and attributes of the Square class"""
+
+    def __init__(self, size):
+        """Method to initialize new object in the Square class
+        Args: size (int): size of Square to create
+        """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
