5006a32b0bcf5eccc63b72bab0d9926d6d242399
ranicholson <ranicho2@gmail.com>
2021-01-19 14:44:47 -0600

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define class BaseGeometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry class methods and attributes"""
+
+    def area(self):
+        """Currently unimplemented method"""
+        raise Exception("area() is not implemented")
