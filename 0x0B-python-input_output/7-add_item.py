5177e6716e13fbf22059d762bb2ecab7cae0ddc5
ranicholson <ranicho2@gmail.com>
2021-01-20 17:49:14 -0600

    Tasks

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Script that adds all arguments to a python list and then saves to a file"""
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def main():
+    """Main function to execute script"""
+
+    try:
+        args = load_from_json_file("add_item.json")
+    except:
+        args = []
+
+    args.extend(sys.argv[1:])
+    save_to_json_file(args, "add_item.json")
