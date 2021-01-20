1685c5f99ca5786d00eb529e4921a46257f70f19
ranicholson <ranicho2@gmail.com>
2021-01-20 13:07:04 -0600

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Function that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Write string to a text file
+    Args:
+        filename (str): filename to write to
+        text (str): string to write to file
+    Return: Number of characters written
+    """
+    chars = 0
+
+    with open(filename, mode="w", encoding="utf-8") as file:
+        return (file.write(text))
