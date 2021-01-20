33f66bb6f618a794ede3015acbfc3491b0a335e2
ranicholson <ranicho2@gmail.com>
2021-01-19 18:58:03 -0600

    Task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,6 +12,6 @@ class Square(Rectangle):
         """Method to initialize new object in the Square class
         Args: size (int): size of Square to create
         """
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
