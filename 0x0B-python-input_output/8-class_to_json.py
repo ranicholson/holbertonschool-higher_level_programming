d0d3b45a3e63a1a53f021089f755350be87de35d
ranicholson <ranicho2@gmail.com>
2021-01-21 14:26:51 -0600

    Task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+"""Returns dictionary description with simple data structure"""
+
+
+def class_to_json(obj):
+    """Returns dicitonary description for JSON serialization"""
+    return (obj.__dict__)
