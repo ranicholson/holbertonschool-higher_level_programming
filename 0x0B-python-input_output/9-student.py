568383788f79b52eda660b94b88a22438fcb1f72
ranicholson <ranicho2@gmail.com>
2021-01-21 20:17:30 -0600

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+"""Class Student definition"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization method"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Method to retrieve dictionary representation of Student instance"""
+
+        return (self.__dict__)
