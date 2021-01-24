fdb39b3d661f649d827034cd6fe36b90b90ecb31
ranicholson <ranicho2@gmail.com>
2021-01-24 16:24:57 -0600

    Task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -33,3 +33,30 @@ class Square(Rectangle):
         """Sets the size of square"""
         self.width = size
         self.height = size
+
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute"""
+        pos = 0
+        if args is not None and len(args) > 0:
+            for arg in args:
+                if pos == 0:
+                    self.id = arg
+                elif pos == 1:
+                    self.width = arg
+                    self.height = arg
+                elif pos == 2:
+                    self.x = arg
+                elif pos == 3:
+                    self.y = arg
+                pos += 1
+        elif kwargs is not None and len(kwargs) > 0:
+            for key, arg in kwargs.items():
+                if key == "id":
+                    self.id = arg
+                elif key == "size":
+                    self.width = arg
+                    self.height = arg
+                elif key == "x":
+                    self.x = arg
+                elif key == "y":
+                    self.y = arg
