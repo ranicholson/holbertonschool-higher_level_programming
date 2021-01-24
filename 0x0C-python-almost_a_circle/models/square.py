b8d22afb5dd20794fcf56af47516ebb90d79e1b1
ranicholson <ranicho2@gmail.com>
2021-01-24 14:47:14 -0600

    Task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Class Square that inherits from Rectangle"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class than inherits from Rectangle which inherits from Base"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializer for Square class
+        Args:
+            size (int): size of Square
+            x (int):
+            y (int):
+            id (int):ID number of Square
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns string with rectangle description"""
+        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                  self.width))
