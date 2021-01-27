90c601b7870108700d74dcb3eb642cf0b96ccf9d
ranicholson <ranicho2@gmail.com>
2021-01-26 19:27:29 -0600

    Base test update

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Unittest for the Base class"""
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class BaseTest(unittest.TestCase):
+    """Tests for the Base class"""
+
+    def test_noid(self):
+        """Test under normal parameters"""
+        bas1 = Base()
+        bas2 = Base()
+        bas3 = Base()
+        self.assertEqual(bas1.id, (bas2.id - 1))
+
+    def test_idprov(self):
+        """Test with id provided and id as None"""
+        bas1 = Base(None)
+        bas2 = Base(20)
+        bas3 = Base()
+        self.assertEqual(bas1.id, (bas3.id - 1))
+        self.assertEqual(bas2.id, 20)
