336fcdd5789e8b7c145477f9d88d52aa40f525ba
ranicholson <ranicho2@gmail.com>
2021-01-27 07:15:37 -0600

    updates

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 
-from models.base import Base
-
 """Subclass Rectangle inherits from Base class. Just adding to pass checker"""
+
+
+from base import Base
 
 
 class Rectangle(Base):
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,7 +2,7 @@
 
 """Class Square that inherits from Rectangle"""
 
-from models.rectangle import Rectangle
+from rectangle import Rectangle
 
 
 class Square(Rectangle):
