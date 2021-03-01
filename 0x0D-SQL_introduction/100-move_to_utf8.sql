56b7f661c0b00d1c488b2568769c10f0a91cabc4
ranicholson <ranicho2@gmail.com>
2021-03-01 14:29:47 -0600

    Task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- Script to convert database to UTF8
-ALTER DATABASE hbtn_0c_0 charset=utf8;
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
