0507b81a07f3b310774e26b01c4a3ef61c46b085
ranicholson <ranicho2@gmail.com>
2021-03-02 12:13:22 -0600

    Task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Script to list all privileges for specified MySQL users
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1@localhost';
+SHOW GRANTS FOR 'user_0d_2@localhost';
