85ff4142c3e56b622a7b60446f25ae4edf2f40a4
ranicholson <ranicho2@gmail.com>
2021-03-02 12:11:18 -0600

    Task 9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Script to list all cities contained in a database
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id=states.id
+ORDER BY cities.id ASC;
