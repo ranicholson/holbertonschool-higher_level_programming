e093a42f8ed33cbe821d71ca42e52e1bcb898782
ranicholson <ranicho2@gmail.com>
2021-03-02 11:10:49 -0600

    Task 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- Script that lists all cities of California in database
+
+SELECT id, name
+FROM cities
+WHERE state_id=
+      (SELECT id
+      FROM states
+      WHERE name="California")
+ORDER BY id;
