9e3e9208bdf17797fbc5a4f389f15e6975242a77
Allen Nicholson <ranicho2@gmail.com>
2021-03-23 15:20:31 -0500

    Task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Function that reverses a list
 
-exports.esrever = function (list){
-    return list.map(list.pop,[...list]);
-}
+exports.esrever = function (list) {
+  return list.map(list.pop, [...list]);
+};
