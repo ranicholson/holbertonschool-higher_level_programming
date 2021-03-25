9db1fbcba0635fac80a1849b1c21511e8ad18247
Allen Nicholson <ranicho2@gmail.com>
2021-03-25 13:28:37 -0500

    Task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Script that lists all states from specified database"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    states = cursor.execute("""SELECT * FROM states where name REGEXP '^[N].*$'\
+                             ORDER BY id ASC""")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+    cursor.close()
+    db.close()
