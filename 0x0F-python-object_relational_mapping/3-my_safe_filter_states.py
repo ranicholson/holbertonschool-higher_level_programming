0374aa62ff5138e3112c29332539bf0d77d47e88
Allen Nicholson <ranicho2@gmail.com>
2021-03-25 14:07:02 -0500

    Task3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Script that lists state based on argument passed"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states where\
+                    name=%(name)s", {'name': argv[4]})
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+    cursor.close()
+    db.close()
