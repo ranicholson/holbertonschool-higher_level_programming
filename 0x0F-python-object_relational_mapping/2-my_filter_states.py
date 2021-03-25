717876b6a8b74cd2af5139ebc4f297f055e4ccce
Allen Nicholson <ranicho2@gmail.com>
2021-03-25 13:38:30 -0500

    Task2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that lists states starting with letter N from specified database"""
+""" Script that lists state based on argument passed"""
 
 
 import MySQLdb
@@ -8,8 +8,8 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    states = cursor.execute("""SELECT * FROM states where name REGEXP '^[N].*$'\
-                             ORDER BY id ASC""")
+    states = cursor.execute("SELECT * FROM states\
+                            where name='{}'".format(argv[4]))
     result = cursor.fetchall()
     for row in result:
         print(row)
