99b6b8912a7f92856772fe065cf3e633b1ccb1be
Allen Nicholson <ranicho2@gmail.com>
2021-03-25 14:32:32 -0500

    Task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Script that lists cities from specified database"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM cities ORDER BY id ASC""")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+    cursor.close()
+    db.close()
