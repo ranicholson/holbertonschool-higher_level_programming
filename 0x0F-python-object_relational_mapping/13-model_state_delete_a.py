08bdc20da8c14a8affedddac6ffd32e12debe720
Allen Nicholson <ranicho2@gmail.com>
2021-03-26 13:56:33 -0500

    Task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Script that lists all state objects from specified database"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            session.delete(state)
+    session.commit()
+    session.close()
