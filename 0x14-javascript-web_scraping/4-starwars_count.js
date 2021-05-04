ec5f9e66dc0622d15c97c199d1753684efde90cf
ranicholson <ranicho2@gmail.com>
2021-05-04 20:04:17 +0000

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,6 +3,8 @@
 
 const request = require('request');
 const URLEpisode = 'https://swapi-api.hbtn.io/api/people/18';
+const URLfilms = 'https://swapi-api.hbtn.io/api/films/';
+request(URLfilms);
 request(URLEpisode, function (err, resp, bod) {
   if (err) {
     console.log(err);
