8164c43523b3bb3dd303d31dd6eec79a0b096a40
ranicholson <ranicho2@gmail.com>
2021-05-06 19:08:16 +0000

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,10 +2,20 @@
 // Script to request status code
 
 const request = require('request');
-request(process.argv[2], function (err, resp, bod) {
+request.get(process.argv[2], function (err, r, body) {
   if (err) {
     console.log(err);
   } else {
-    console.log(3);
+    let WedgeCount = 0;
+    const films = JSON.parse(body).results;
+    for (let x = 0; x < films.length; x++) {
+      for (let y = 0; y < films[x].characters.length; y++) {
+        if (films[x].characters[y].includes('/18/')) {
+          WedgeCount++;
+          break;
+        }
+      }
+    }
+    console.log(WedgeCount);
   }
 });
